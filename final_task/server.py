08955dce20d06a3ae70be57006e0008d074789a8
DmitriyReztsov <rezcov_d@mail.ru>
2022-01-28 12:25:03 +0300

    async and multiple processing

--- final_task/server.py
+++ final_task/server.py
@@ -1,7 +1,7 @@
 import asyncio
 import os
 import subprocess
-from contextlib import contextmanager
+import sys
 
 import websockets
 from dotenv import load_dotenv
@@ -10,45 +10,29 @@ load_dotenv()
 TIMEOUT = int(os.getenv("TIMEOUT")) if os.getenv("TIMEOUT") else 10
 
 
-@contextmanager
-def file_open(path):
-    try:
-        yield
-    finally:
-        os.remove(path)
-
-
 async def execution(websocket, text):
-    with open("script.py", "w", encoding="utf8") as file:
-        file.write(text)
-        path = os.path.abspath(file.name)
+    proc = await asyncio.create_subprocess_exec(
+        sys.executable, "-u", "-c", text, stdout=subprocess.PIPE, stderr=subprocess.PIPE
+    )
 
-    with file_open(path):
-        proc = subprocess.Popen(  # -c
-            ["python", "-u", path],
-            stdout=subprocess.PIPE,
-            stderr=subprocess.PIPE,
-            text=True,
-            encoding="utf8",
-        )
-        for line in iter(proc.stdout.readline, ""):
-            line = line.rstrip()
-            await websocket.send(line)
+    async for line in proc.stdout:
+        await websocket.send(line.decode("utf8"))
 
-        for line in iter(proc.stderr.readline, ""):
-            line = line.rstrip()
-            await websocket.send(line)
+    async for line in proc.stderr:
+        await websocket.send(line.decode("utf8"))
 
 
 async def start_server(websocket):
     async for text in websocket:
         await websocket.send("start execution")
         print(f">>> {text} <<<")
-        text += "import sys\n" \
-            "sys.modules['os'] = None\n" \
-            "del __builtins__.open\n" \
-            "del __builtins__.exec\n" \
-            "del __builtins__.eval\n\n"
+        text = (
+            "import sys\n"
+            "sys.modules['os'] = None\n"
+            "del __builtins__.open\n"
+            "del __builtins__.exec\n"
+            "del __builtins__.eval\n\n" + text
+        )
         try:
             await asyncio.wait_for(execution(websocket, text), timeout=TIMEOUT)
         except asyncio.TimeoutError:
